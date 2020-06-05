0ea2b592ca3c550a092da5664bf1426e7c095176
Shaked Ashkenazi <shaked.a21@gmail.com>
2020-06-05 11:38:26 +0300

    Remove padding from segment

--- src/layouts/HeaderBar.jsx
+++ src/layouts/HeaderBar.jsx
@@ -1,20 +1,16 @@
-import React from 'react';                                                
-import {                                                                                  
-  Image, Header,                                                                          
-} from 'semantic-ui-react';                                                               
+import React from "react";
+import { Image, Header } from "semantic-ui-react";
 
-const HeaderBar = () => {                                                                 
-  return (                                                                                
-    <div>                                                                                 
-      <Header as="h5" floated="left">                                                     
-        <Header.Content>                                                                  
-          <Image src={''} avatar circular />                      
-          <span>                                                                          
-            Sudoku solver
-          </span>                                                                         
-        </Header.Content>                                                                 
-      </Header>                                                                           
-    </div>                                                                                
-  );                                                                                      
-};                                                                                        
-export default HeaderBar;            
+const HeaderBar = () => {
+  return (
+    <div>
+      <Header as="h5" floated="left">
+        <Header.Content>
+          <Image src={""} avatar circular />
+          <span>Sudoku solver</span>
+        </Header.Content>
+      </Header>
+    </div>
+  );
+};
+export default HeaderBar;
